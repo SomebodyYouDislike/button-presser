912a991cb5a15c65e8924256c35d4e30f0b79a4c
SomebodyYouDislike <61160638+SomebodyYouDislike@users.noreply.github.com>
2020-11-30 16:06:05 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+input.onButtonPressed(Button.A, function () {
+    if (true) {
+        Item += 1
+        basic.showNumber(Item)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (true) {
+        Item = 0
+        basic.showNumber(Item)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (true) {
+        Item += -1
+        basic.showNumber(Item)
+    }
+})
+let Item = 0
+Item = 0
+basic.forever(function () {
+	
+})
